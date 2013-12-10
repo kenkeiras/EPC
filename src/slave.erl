ebd27eb562a163bd7db74454e0793bf7b677f449
DaniMunin <danmunin@gmail.com>
2013-12-10 20:01:22 +0100

    First version of Slave Module. It needs testing

--- src/slave.erl
+++ src/slave.erl
@@ -1,33 +1,36 @@
 -module(slave).
--export([init/0, crawlURL/1]).
--export([loop/0, indexImageList/1]).
+-export([init/1]).
 
-crawlURL(WebUrl) ->
-    slave ! {self(), {crawl, ImageUrl}}.
-	
-indexImageURL(ImageUrlList) ->
-    slave ! {self(), {index, ImageUrlList}}.
+init(Url) ->
+    crawl(Url),
+    ok.
 
-loop() ->
-    receive
-        {_From, {crawl, webUrl}} ->
-            webLib:getImages(webUrl),
-            loop();
-		    {_From, {index, ImageUrlList}} ->
-            indexImageList(indexImageList),
-            loop()
+crawl(Url)->
+    {CrawledUrls, OtherDomainUrls, ImageLinks} = crawl(Url,[],[],[],[]),
+    indexImages(ImageLinks),
+    sendNewUrls(CrawledUrls,OtherDomainUrls).
+
+crawl(Url,CurrentDomainNotCrawledUrls,CurrentDomainCrawledUrls,OtherDomainUrls,ImageUrls) ->
+   {WebLinks,NewImageUrls} = web_lib:get_data(Url),
+   CurrentDomainUrls = [ Link || Link <- WebLinks, belongsToDomain(Link,Url)],
+   OtherUrls = [ Link || Link <- WebLinks, not belongsToDomain(Link,Url)],
+   CurrentDomainNotCrawledUrls =  CurrentDomainNotCrawledUrls ++ master:removeDuplicatedURLs(CurrentDomainUrls,CurrentDomainCrawledUrls),
+
+   case CurrentDomainNotCrawledUrls of
+        [Head | Tail] -> 
+            crawl(Head,Tail,[Head | CurrentDomainCrawledUrls],[OtherDomainUrls ++ OtherUrls],[ImageUrls ++ NewImageUrls]);
+        [] -> 
+            {[Url | CurrentDomainCrawledUrls],[OtherDomainUrls ++ OtherUrls], [ImageUrls ++ NewImageUrls] }
     end.
 
-indexImageList([ImageUrl|T]) ->
-    indexer:indexImage(ImageUrl), 
-	indexImageList(T);
-indexImageList([ImageUrl|[]]) ->
-    indexer:indexImage(ImageUrl).
+% Depends on  the input format(Lastra) + parser
+belongsToDomain(String,Domain) ->
+    string:equal(Domain,string:sub_string(String,1,string:len(Domain))). 
 
-slave() ->
-   register(slave, self()),
-   loop().
+indexImages(ImageList) ->
+    lists:map(indexer:indexImage(),ImageList). 
 
-init() ->
-    spawn(?MODULE, slave, []),
-    ok.
+sendNewUrls(Crawled,NewUrls) ->
+    master ! {foundURLs, self(), {Crawled,NewUrls}}.    
+
+
