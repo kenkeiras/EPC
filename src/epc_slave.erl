a91a57b68bc88d8315b5293def85e0435e371623
kenkeiras <kenkeiras@gmail.com>
2013-12-17 20:55:02 +0100

    Renamed slave, to avoid conflicts with erlang's one

--- src/epc_slave.erl
+++ src/epc_slave.erl
@@ -1,4 +1,4 @@
--module(slave).
+-module(epc_slave).
 -export([init/1]).
 
 init(Url) ->
@@ -11,26 +11,27 @@ crawl(Url)->
     sendNewUrls(CrawledUrls,OtherDomainUrls).
 
 crawl(Url,CurrentDomainNotCrawledUrls,CurrentDomainCrawledUrls,OtherDomainUrls,ImageUrls) ->
+    io:format("= ~p~n", [Url]),
    {WebLinks,NewImageUrls} = web_lib:get_data(Url),
    CurrentDomainUrls = [ Link || Link <- WebLinks, belongsToDomain(Link,Url)],
    OtherUrls = [ Link || Link <- WebLinks, not belongsToDomain(Link,Url)],
    CurrentDomainNotCrawledUrls =  CurrentDomainNotCrawledUrls ++ master:removeDuplicatedURLs(CurrentDomainUrls,CurrentDomainCrawledUrls),
 
    case CurrentDomainNotCrawledUrls of
-        [Head | Tail] -> 
+        [Head | Tail] ->
             crawl(Head,Tail,[Head | CurrentDomainCrawledUrls],[OtherDomainUrls ++ OtherUrls],[ImageUrls ++ NewImageUrls]);
-        [] -> 
+        [] ->
             {[Url | CurrentDomainCrawledUrls],[OtherDomainUrls ++ OtherUrls], [ImageUrls ++ NewImageUrls] }
     end.
 
 % Depends on  the input format(Lastra) + parser
 belongsToDomain(String,Domain) ->
-    string:equal(Domain,string:sub_string(String,1,string:len(Domain))). 
+    io:format("::~p - ~p~n", [String, Domain]),
+    string:equal(Domain,string:sub_string(String,1,string:len(Domain))).
 
 indexImages(ImageList) ->
-    lists:map(indexer:indexImage(),ImageList). 
+    io:format("~p~n", [ImageList]),
+    lists:map(fun (X) -> indexer:indexImage(X) end,ImageList).
 
 sendNewUrls(Crawled,NewUrls) ->
-    master ! {foundURLs, self(), {Crawled,NewUrls}}.    
-
-
+    master ! {foundURLs, self(), {Crawled,NewUrls}}.
