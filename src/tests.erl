4056ff689756591fd6ec6a9b0ec18d4921a53396
kenkeiras <kenkeiras@gmail.com>
2014-01-11 19:37:59 +0100

    Start more things on tests:test() :
      * Install mnesia DB if not installed yet.
      * Start web interface.

--- src/tests.erl
+++ src/tests.erl
@@ -1,6 +1,13 @@
 -module(tests).
 -export([test/0]).
 
+try_to_install() ->
+    try epc_dba:install([node()]) of
+        ok -> ok
+    catch _:{badmatch, {error, {_, {already_exists, _}}}} ->
+            ok
+    end.
+
 test1()->
     master:getImages(["http://elpais.com/"]).
 
@@ -8,5 +15,7 @@ test()->
     master:init(),
     indexer:start(),
     phash:start(),
+    try_to_install(),
     epc_dba:start([node()]),
+    web:init(),
     test1().
